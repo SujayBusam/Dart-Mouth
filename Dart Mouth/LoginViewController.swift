30739372df90f08294ce7baad5298ba8133f0d3b
Sujay Busam <sujay.busam@gmail.com>
2016-01-03 18:13:48 -0500

    Implement signin validation

--- Dart Mouth/LoginViewController.swift
+++ Dart Mouth/LoginViewController.swift
@@ -11,9 +11,25 @@ import UIKit
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
     struct Identifiers {
-        static let dismissKeyboard = "dismissKeyboard"
+        static let DismissKeyboard = "dismissKeyboard"
     }
     
+    struct Validation {
+        static let MinimumPasswordLength = 6
+        static let MaximumPasswordLength = 25
+        static let EmailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        
+        static let InvalidEmailTitle = "Invalid Email"
+        static let InvalidEmailMessage = "Please sign up with a valid email."
+        static let InvalidPasswordTitle = "Invalid Password"
+        static let InvalidPasswordMessage = "Please enter a password between \(MinimumPasswordLength) and \(MaximumPasswordLength) characters, inclusive."
+        static let NoMatchPasswordsTitle = "Passwords Don't Match"
+        static let NoMatchPasswordsMessage = "Please correctly confirm your password."
+        static let OkActionTitle = "OK"
+    }
+    
+    var alertView = UIAlertController(title: nil, message: nil, preferredStyle: .Alert)
+    var alertAction = UIAlertAction(title: Validation.OkActionTitle, style: .Default, handler: nil)
     
     // MARK: - Text field outlets
     
@@ -37,8 +53,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         // Looks for single or multiple taps
         let tap = UITapGestureRecognizer(target: self,
-            action: NSSelectorFromString(Identifiers.dismissKeyboard))
+            action: NSSelectorFromString(Identifiers.DismissKeyboard))
         view.addGestureRecognizer(tap)
+        
+        alertView.addAction(alertAction)
     }
     
     
@@ -50,10 +68,14 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    // MARK: - Butßton actions
+    // MARK: - Button actions
     
     @IBAction func signupButtonPressed(sender: UIButton) {
-        
+        let allFieldsValid = !showAlertsForInvalidFields() // intentionally being explicit here
+        if allFieldsValid {
+            // Sign up
+            print("Signup")
+        }
     }
     
     @IBAction func loginButtonPressed(sender: UIButton) {
@@ -66,6 +88,49 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     func dismissKeyboard() {
         // Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+    
+    
+    // MARK: - Validation helper function
+    
+    func isValidEmail(email: String) -> Bool {
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", Validation.EmailRegex)
+        return emailTest.evaluateWithObject(email)
+    }
+    
+    func isValidPassword(password: String) -> Bool {
+        let len = password.characters.count
+        return len >= Validation.MinimumPasswordLength && len <= Validation.MaximumPasswordLength
+    }
+    
+    /*
+    Helper function that shows at most one alert for an invalid email, password, or password
+    confirmation field. Returns true if an alert was shown. Returns false if all fields are valid.
+    */
+    func showAlertsForInvalidFields() -> Bool {
+        let email: String? = emailTextField.text
+        let password: String? = passwordTextField.text
+        let confirmPassword: String? = confirmPasswordTextField.text
+        
+        if email == nil || !isValidEmail(email!) {
+            alertView.title = Validation.InvalidEmailTitle
+            alertView.message = Validation.InvalidEmailMessage
+            self.presentViewController(alertView, animated: true, completion: nil)
+            return true
+        }
+        if password == nil || !isValidPassword(password!) {
+            alertView.title = Validation.InvalidPasswordTitle
+            alertView.message = Validation.InvalidPasswordMessage
+            self.presentViewController(alertView, animated: true, completion: nil)
+            return true
+        }
+        if confirmPassword == nil || password! != confirmPassword! {
+            alertView.title = Validation.NoMatchPasswordsTitle
+            alertView.message = Validation.NoMatchPasswordsMessage
+            self.presentViewController(alertView, animated: true, completion: nil)
+            return true
+        }
+        return false
     }
 
     /*
