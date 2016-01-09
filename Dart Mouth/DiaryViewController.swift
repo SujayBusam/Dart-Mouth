ed08a7db03072d73ff22cafb0003c137fcaaa0a1
Sujay Busam <sujay.busam@gmail.com>
2016-01-09 17:00:13 -0500

    Add functioning date navigation control to navigation bar

--- Dart Mouth/DiaryViewController.swift
+++ Dart Mouth/DiaryViewController.swift
@@ -8,11 +8,67 @@
 
 import UIKit
 
-class DiaryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class DiaryViewController: UIViewController, DateNavigationControlDelegate {
+    
+    // MARK: - Local Constants
+    
+    private struct Dimensions {
+        static let NavBarItemHeight: CGFloat = 35
+        static let DateNavControlWidth: CGFloat = 190
     }
 
+    
+    // MARK: - Instance variables
+    
+    // The current menu date.
+    var date: NSDate = NSDate() {
+        didSet { updateUI() }
+    }
+    
+    var dateNavigationControl: DateNavigationControl! {
+        didSet { dateNavigationControl.delegate = self }
+    }
+    
+    
+    // MARK: - Controller / View Setup
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        updateUI()
+    }
+    
+    func updateUI() {
+        dateNavigationControl.updateDateLabel()
+    }
+    
+    private func setupViews() {
+        // Create and setup date navigation control
+        dateNavigationControl = DateNavigationControl(frame: CGRectMake(0, 0, Dimensions.DateNavControlWidth, Dimensions.NavBarItemHeight))
+        self.navigationItem.titleView = dateNavigationControl
+    }
+    
+    
+    // MARK: - DateNavigationControlDelegate protocol methods
+    
+    func dateForDateNavigationControl(sender: DateNavigationControl) -> NSDate {
+        return self.date
+    }
+    
+    func leftArrowWasPressed(sender: UIButton) {
+        if let dayBefore = NSCalendar.currentCalendar().dateByAddingUnit(.Day, value: -1, toDate: self.date, options: []) {
+            self.date = dayBefore
+        } else {
+            print("Date decrement calculation failed")
+        }
+    }
+    
+    func rightArrowWasPressed(sender: UIButton) {
+        if let dayAfter = NSCalendar.currentCalendar().dateByAddingUnit(.Day, value: 1, toDate: self.date, options: []) {
+            self.date = dayAfter
+        } else {
+            print("Date increment calculation failed")
+        }
+    }
 }
