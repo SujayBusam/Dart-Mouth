3125e479796145247a4393f9d85b22de0ae0f4d3
Sujay Busam <sujay.busam@gmail.com>
2016-02-20 16:30:47 -0500

    Implement previous meal entry cell

--- Dart Mouth/PreviousMealEntryTableViewCell.swift
+++ Dart Mouth/PreviousMealEntryTableViewCell.swift
@@ -7,18 +7,66 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class PreviousMealEntryTableViewCell: UITableViewCell {
 
+    // MARK: - Outlets
+    // This is for ONE diary entry. The current cell.
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var servingsLabel: UILabel!
+    @IBOutlet weak var totalCaloriesLabel: UILabel!
+    @IBOutlet weak var checkBoxButton: CheckboxUIButton!
+    
+    
+    // MARK: - Instance variables
+    
+    var diaryEntry: DiaryEntry? {
+        didSet { updateUI() }
+    }
+    
+    var isChecked: Bool {
+        return self.checkBoxButton.isChecked
+    }
+    
+    var numberFormatter: NSNumberFormatter = NSNumberFormatter()
+    var fracUtil = FractionUtil()
+    
+    // MARK: - Default override functions
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         // Initialization code
+        self.numberFormatter.numberStyle = .DecimalStyle
+        self.totalCaloriesLabel.textColor = FlatNavyBlue()
+        self.servingsLabel.textColor = FlatGrayDark()
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+    
+    
+    // MARK: - UI Configuration
+    
+    func updateUI() {
+        // TODO: may have to reset any existing diary entry information here.
+        if let diaryEntry = self.diaryEntry {
+            let recipe = diaryEntry.recipe
+            let multiplier = diaryEntry.servingsMultiplier
+            
+            var servingsText = "Serving"
+            if multiplier > 1.0 {
+                servingsText = "Servings"
+            }
+            
+            recipeNameLabel?.text = recipe.name
+            servingsLabel?.text = "\(fracUtil.vulgarFraction(Double(multiplier)).0) " + servingsText
+            totalCaloriesLabel?.text = self.numberFormatter.stringFromNumber(diaryEntry.getTotalCalories()!)
+        }
     }
 
 }
